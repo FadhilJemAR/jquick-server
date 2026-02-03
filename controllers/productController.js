ddc437b59cea3d72a84d099d37f10f24b399d9bf
Fadhil <fadhilar91128@gmail.com>
2026-02-03 15:33:10 +0800

    buat controller untuk  produk dan route untuk produk serta model datanya

---
+++ controllers/productController.js
@@ -0,0 +1,34 @@
+import Product from "../models/Product";
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, category, stock, imageUrl } = req.body;
+
+    const product = new Product({
+      name,
+      description,
+      price,
+      category,
+      stock,
+      imageUrl,
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json({message:'Berhasil menambahkan produk',createdProduct});
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Gagal menambahkan produk", error: error.message });
+  }
+};
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find().lean();
+    res.json({message:'Berhasil mengambil produk',products});
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Gagal mengambil produk", error: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import dbConnect from "./lib/dbConnect";
+import productRouter from './routes/productRoutes'
 dotenv.config();
-
+dbConnect();
 const app = express();
 const PORT = process.env.SERVER_PORT;
 
+app.use(productRouter)
 
-app.use(cors({origin:process.env.ALLOWED_ORIGIN}));
-
-
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
