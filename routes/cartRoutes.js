2d312675999737ef4f1694d69f7c8d19e127c8b3
Fadhil <fadhilar91128@gmail.com>
2026-02-16 20:15:52 +0800

    ubah route path controller changeQuanityProductInCart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -5,5 +5,5 @@ const router = express.Router({mergeParams:true});
 router.post('/add',addProductUserCart);
 router.get('/',getProductsInCart);
 router.delete('/remove/:productId',deleteProductFromCart);
-router.patch('/change/:productId',changeQuantityProductInCart);
+router.patch('/change',changeQuantityProductInCart);
 export default router
